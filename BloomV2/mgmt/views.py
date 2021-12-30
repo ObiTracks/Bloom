8c5c8dc98dcf8e125b9cebf35f20493c48d869e9
Obi Ihejirika <oihejirika@Obis-MacBook-Air.local>
2021-12-30 17:20:17 -0500

    Amenity object page created

--- BloomV2/mgmt/views.py
+++ BloomV2/mgmt/views.py
@@ -9,8 +9,17 @@ def dashboard_view(request):
     template_name = '../templates/pages/dashboard.html'
     return render(request, template_name, context)
 
+
 def amenityhub_view(request):
     page_title = "Amenity Hub"
     context = {'page_title': page_title}
     template_name = '../templates/pages/amenityhub.html'
+    return render(request, template_name, context)
+
+
+def amenityobject_view(request):
+    page_title = "Amenity Name"
+    page_subtitle = "Amenities"
+    context = {'page_title': page_title, 'page_subtitle': page_subtitle}
+    template_name = '../templates/pages/amenityobject.html'
     return render(request, template_name, context)

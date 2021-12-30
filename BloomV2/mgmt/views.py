0a429bafa30176e79f7cac5fbf06fb21ede7c3cf
Obi Ihejirika <oihejirika@Obis-MacBook-Air.local>
2021-12-30 16:37:51 -0500

    Amenity view and page created, and stat badge added to base.html

--- BloomV2/mgmt/views.py
+++ BloomV2/mgmt/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def dashboard(request):
+def dashboard_view(request):
     page_title = "Main Dashboard"
     context = {'page_title': page_title}
     template_name = '../templates/pages/dashboard.html'
+    return render(request, template_name, context)
+
+def amenityhub_view(request):
+    page_title = "Amenity Hub"
+    context = {'page_title': page_title}
+    template_name = '../templates/pages/amenityhub.html'
     return render(request, template_name, context)
